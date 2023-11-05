e82a36557ce921eee6f8e37198cf48e85e16dc66
Lakshay <93575614+lakshayk09@users.noreply.github.com>
2023-11-05 22:23:20 +0530

    Delete starter.js

--- starter.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $(document).scroll(function () {
-        var $nav_height = $("#starter")
-        var $nav = $("#mainNavbar");
-        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav_height.height());
-    });
-});
