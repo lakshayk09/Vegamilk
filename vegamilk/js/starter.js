304546742ce2b5dd7dba1d3aa3e9bf3cb85e7c7f
Lakshay <93575614+lakshayk09@users.noreply.github.com>
2023-11-08 19:50:39 +0530

    Delete vegamilk directory

--- vegamilk/js/starter.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $(document).scroll(function () {
-        var $landing_page = $("#starter")
-        var $nav = $("#mainNavbar");
-        $nav.toggleClass("scrolled", $(this).scrollTop() > $landing_page.height());
-    });
-});
